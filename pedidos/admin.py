ee9f0b3e3b067132bedcd7bfd6a781091e196228
Karen Avila <kl.avila@ebc.edu.mx>
2026-08-13 21:24:07 -0600

    Agrega pedidos, modelos, vistas y templates

---
+++ pedidos/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Producto, Pedido
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'precio', 'categoria', 'disponible')
+    list_filter = ('categoria', 'disponible')
+    search_fields = ('nombre',)
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente_nombre', 'estado', 'total', 'fecha')
+    list_filter = ('estado', 'fecha')
+    search_fields = ('cliente_nombre',)
+# Register your models here.
